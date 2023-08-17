f0a5cd2be7154b3f2007194471eb601041c8657a
Rafal Chlodnicki <rchl2k@gmail.com>
2023-08-17 08:41:57 +0200

    fix module reference

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -30,7 +30,7 @@ const config: NuxtConfig = {
     buildModules: [
         // https://go.nuxtjs.dev/typescript
         '@nuxt/typescript-build',
-        'volar-component-types',
+        'volar-component-types/nuxt',
     ],
 
     modules: [
